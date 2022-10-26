4d8acf1dfa5316b471f59dd7464333f4101fc62e
EduardoLevandoski <eduardolevandoski@outlook.com>
2022-10-26 20:55:48 -0300

    pop method optinal value implementation

--- lib/utils/nav.dart
+++ lib/utils/nav.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 //Future
 push(BuildContext context, Widget page) => Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) => page));
 
-pop(BuildContext context, String text) => Navigator.pop(context, text);
+pop(BuildContext context, [var value]) => Navigator.pop(context, value);
+
+
