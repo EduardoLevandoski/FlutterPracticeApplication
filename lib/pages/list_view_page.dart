3d851fb8f63e78e1b22a7192a60f85a12b6b3adb
Eduardo Levandoski <eduardo.levandoski@systempro.com.br>
2022-10-27 09:04:40 -0300

    list_view_page implementation with list view builder

--- lib/pages/list_view_page.dart
+++ lib/pages/list_view_page.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/material.dart';
 
+class Dog {
+  String name;
+  String picture;
+
+  Dog(this.name, this.picture);
+
+  getPicture() => _img(picture);
+
+}
+
 class ListViewPage extends StatelessWidget {
   const ListViewPage({Key? key}) : super(key: key);
 
@@ -11,6 +21,32 @@ class ListViewPage extends StatelessWidget {
         backgroundColor: Colors.orange,
         centerTitle: true,
       ),
+      body: _body(),
     );
   }
+}
+
+_body() {
+  List<Dog> dogs = [
+    Dog("Jack Russel", "dog1.png"),
+    Dog("Golden Retriever", "dog2.png"),
+    Dog("Pug", "dog3.png"),
+    Dog("Rottweiler", "dog4.png"),
+    Dog("Border Collie", "dog5.png"),
+  ];
+
+  return ListView.builder(
+      itemCount: dogs.length,
+      itemExtent: 420,
+      itemBuilder: (context, index) => dogs[index].getPicture());
+}
+
+_img(String img) {
+  return Container(
+    padding: const EdgeInsets.only(top: 20),
+    child: Image.asset(
+      "assets/images/" + img,
+      fit: BoxFit.cover,
+    ),
+  );
 }
